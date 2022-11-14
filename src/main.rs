f9efcbdcbc4e07faba6f2d5f6c015ddebcc30034
boris <boris.lok@cienet.com>
2022-11-14 14:50:55 +0800

    [Feature] Simple exmaple of axum.
    Make a `hello_world` example.
    
    Steps:
    1. ```cargo_run```
    2. ```curl --request GET 127.0.0.1:3000 ```

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,22 @@
-fn main() {
+use axum::routing::get;
+use axum::Router;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    println!("listening on {}", &addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> &'static str {
+    "hello world"
 }
