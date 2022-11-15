ce677a12d3f08730eb843c272201259b6f753999
boris <boris.lok@cienet.com>
2022-11-15 10:52:50 +0800

    [Feature] Add configuration. Seperate the `local` and `production` environment.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
-use axum::routing::get;
+use std::net::TcpListener;
+
 use axum::Router;
+use axum::routing::get;
+
+use japonfou::configuration::get_configuration;
 use japonfou::routes::health_check;
-use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
     let app = Router::new().route("/health_check", get(health_check));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let configuration = get_configuration().expect("Can't get configuration");
 
-    println!("listening on {}", &addr);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
 
-    axum::Server::bind(&addr)
+    let listener = TcpListener::bind(&address)
+        .expect(format!("Can't bind address {} to TcpListener", &address).as_str());
+
+    println!("listening on {:?}", listener);
+
+    axum::Server::from_tcp(listener)
+        .expect("Axum can't bind tcp listener")
         .serve(app.into_make_service())
         .await
-        .unwrap();
+        .unwrap()
 }
