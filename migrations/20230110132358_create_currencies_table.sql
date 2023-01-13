8ecf308d8c69b3710b92e1e31cb9908ff6be35ec
boris <boris.lok@cienet.com>
2023-01-13 09:48:44 +0800

    [Feature] Add default value for currencies.

--- migrations/20230110132358_create_currencies_table.sql
+++ migrations/20230110132358_create_currencies_table.sql
@@ -5,4 +5,8 @@ create table currencies
     id   smallint   not null,
     name varchar(8) not null,
     primary key (id)
-)
+);
+
+insert into currencies (id, name)
+values (446, 'MOP'),
+       (344, 'HKD');
